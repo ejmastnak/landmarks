80628ba4cb4c2a9dda78c5278eb58926310c34bc
Elijan Mastnak <ejmastnak@gmail.com>
2023-02-23 09:35:27 +0100

    feat: add a custom 503 error page for maintenance mode
    ...and copied all the default error templates along with it.

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::minimal')
+
+@section('title', __('Check Back Soon!'))
+@section('code', '503')
+@section('message', __('Unavailable due to maintenance'))
+@section('details', __('Check back soon—the website is being updated and should be live within a few minutes.'))
